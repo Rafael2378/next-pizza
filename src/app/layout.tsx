ea6b7b7e7fb24497acdfb7430d2a646cd68e5aae
Rafael2378 <rafaelstepanyan036@gmail.com>
2025-04-15 21:39:12 +0400

    uptade

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const nunito = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin", "cyrilic"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+  display: "swap"
+});
+
+export const metadata: Metadata = {
+  title: "Next-pizza",
+  description: "вкусней уже некуда",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/logo.png" type="image/png" />
+      </head>
+      <body
+        className={nunito.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
